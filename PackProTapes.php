c3d0cf930a28aa654f6f23b11d5c9867d105f55d
unknown-dev-257 <unknownforeveryone257@gmail.com>
2024-03-11 15:03:50 +0530

    created pack pro tapes page

---
+++ PackProTapes.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Pack Pro Tapes">
+    <meta property="og:title" content="Pack Pro Tapes">
+    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
+
+    <title>Pack Pro Tapes</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+    <!-- Bootstrap core css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
+    <!-- Custom styles for this site -->
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/media.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'header.php';?>
+    <div class="page-hero pageheade" style="background: #eee url(../img/Pack-Pro-Banner.jpg); height: 350px; background-size: cover; background-position: center;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Pack Pro Tapes</h1>
+          </div>
+          <div class="col-md-12">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/"><i class="fa fa-home"></i> Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">PackProTapes</li>
+              </ol>
+            </nav>
+          </div> 
+        </div>
+      </div>
+    </div>
+    <section class="body__content padding page-body-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="text-center mb-4">
+              <h3 class="wow fadeInUp">Tape it Right, Everytime!</h3>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-10 offset-md-1">
+            <div class="body-content text-center">
+              <p class="wow fadeInUp mt-0 pt-0">Welcome to Packpro Tapes, a premium division of Prem Industries India Limited! Our extensive range of high-quality packaging tapes is designed to meet the diverse packaging needs of various industries. Explore our specialized tapes, including the popular Ajio tapes, ensuring secure and reliable packaging for your products. Trust in the strength and durability of our Amazon Prime tapes, specifically crafted for optimal performance in shipping and storage. <br> We take pride in our commitment to environmental responsibility with our APEO-free tape, providing a sustainable solution without compromising on effectiveness. DS Group tape exemplifies our dedication to meeting industry standards, offering a reliable choice for a wide array of applications. For fashion-forward packaging solutions, discover our H&M tape, combining style and functionality. <br> At Packpro Tapes, we prioritize innovation and excellence to ensure your packaging needs are met with precision. Explore our diverse tape offerings, each catering to specific requirements, and experience the reliability and quality that set us apart in the packaging industry. Elevate your packaging game with Packpro Tapes – your trusted partner in efficient and secure packaging solutions.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <h1 class="text-center mb-5" style="text-transform: uppercase">Our Products</h1>
+    <div class="e-commerce-gallery BoxesGallery">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Ajio.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Ajio.png" alt="">
+                <h4>Ajio Tapes</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Aluminium-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Aluminium-Tape.png" alt="">
+                <h4>Aluminium Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Amazon-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Amazon-Tape.png" alt="">
+                <h4>Amazon Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/APEO-Free-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/APEO-Free-Tape.png" alt="">
+                <h4>APEO Free Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Cross-Filament-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Cross-Filament-Tape.png" alt="">
+                <h4>Cross Filament Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/DS-Group.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/DS-Group.png" alt="">
+                <h4>DS Group Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Glass-Cloth-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Glass-Cloth-Tape.png" alt="">
+                <h4>Glass Cloth Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/H-&-M-Tapes.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/H-&-M-Tapes.png" alt="">
+                <h4>H&M Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Lables.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Lables.png" alt="">
+                <h4>Lables</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Maskting-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Maskting-Tape.png" alt="">
+                <h4>Maskting Tape</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/Tapes/Stop-Tape.png" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/Tapes/Stop-Tape.png" alt="">
+                <h4>Stop Tape</h4>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="ContactPrem pt-0 mt-4">
+      <div class="container">
+        <div class="ContactPrem-content">
+          <h3>Connect to know us better!</h3>
+          <p>Let’s discuss a blend of your products and our packaging.</p>
+          <a href="contact-us.php" class="more-btn mt-4"><i class="fa fa-clone" aria-hidden="true"></i> Contact Us</a>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'footer.php';?>
+</body>
+</html>
