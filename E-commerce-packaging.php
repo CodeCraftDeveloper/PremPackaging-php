8e78b788185dbe0fc0e9612b60c12f652f6a4b34
unknown-dev-257 <unknownforeveryone257@gmail.com>
2024-04-11 12:40:59 +0530

    updated meta title and discription of E-commerce-packaging route

--- E-commerce-packaging.php
+++ E-commerce-packaging.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Prem Packaging">
+    <meta name="description" content="Discover top-quality ecommerce packaging solutions designed to elevate your brand's shipping experience. Explore our range today! ">
     <meta property="og:title" content="Prem Packaging">
     <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>E-Commerce Packaging Solutions</title>
+    <title>Premium Ecommerce Packaging Solutions | Shop Now</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
     <!-- Bootstrap core css -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
