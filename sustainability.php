c5b102963b08df468e8ee4a0bb3d080cd4b742f5
unknown-dev-257 <unknownforeveryone257@gmail.com>
2024-05-07 10:59:44 +0530

    Updated META: corrugated-containers, displays, sustainability

--- sustainability.php
+++ sustainability.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Prem Packaging">
-    <meta property="og:title" content="Prem Packaging">
+    <meta name="description" content="Discover eco-friendly packaging options at Prem Industries India. Reduce environmental impact with our sustainable solutions. Join us in preserving the planet! Explore now.">
+    <meta property="og:title" content="Sustainable Packaging Solutions | Prem Industries India Limited">
     <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>Sustainability</title>
+    <title>Sustainable Packaging Solutions</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
     <!-- Bootstrap core css -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
