b8349956e453b37d454568f6ab1e122f063303c1
unknown-dev-257 <ecommerce@premindustries.in>
2024-10-21 15:02:51 +0530

    e-com sol & sustainability page content and META change

--- sustainability.php
+++ sustainability.php
@@ -59,7 +59,7 @@
       <div class="row mt-60">
         <div class="col-md-6 order2">
           <div class="body-content">
-            <p class="wow fadeInUp">As we work towards creating the best customer experience, we also ensure that sustainable alternatives are offered. We are a tailored company with mindful efforts toward decreasing our carbon footprint. At Prem Industries, we constantly innovate towards sustainability, building a host of eco-friendly products made from recycled materials. We advocate responsible functioning, which ensures that the footsteps of sustainable development are followed thoughtfully.</p>
+            <p class="wow fadeInUp">As we work towards creating the best customer experience, we also ensure that alternatives to sustainable packaging solutions are offered. We are a tailored company with mindful efforts toward decreasing our carbon footprint. At Prem Industries, we constantly innovate towards sustainability, building a host of eco-friendly products made from recycled materials. We advocate responsible functioning, which ensures that the footsteps of sustainable development are followed thoughtfully.</p>
             <p> Our team undertakes environmentally conscious measures which are implemented along with the suppliers, technicians, business partners, etc. Out of the many measures we take, here are some:</p>
             <ul class="listing-content">
               <li><i class="fa fa-arrow-right"></i> We follow a circular economy, by mixing the leftover pieces that are considered waste in our recycling machinery. Our recycling machines produce a lot of raw material, which accelerates the greening needs of our planet, which can’t be neglected.</li>
