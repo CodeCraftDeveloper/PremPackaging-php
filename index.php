690de9710428315f9d2755db065eebbca9b6d51c
unknown-dev-257 <ecommerce@premindustries.in>
2024-10-24 12:21:39 +0530

    index META and title

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Prem Industries India Limited is the best packaging company in India, we provide the best packaging solutions like corrugated boxes, pouches, rigid boxes, labels, and tapes.">
+    <meta name="description" content="Prem Industries India Limited is the best packaging company in India, we provide the best packaging solutions like corrugated boxes, pouches, rigid boxes and labels">
     <meta property="og:title" content="Prem Industries India Limited | Best Packaging Company in India">
     <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
 
-    <title>Prem Industries India Limited | Best Packaging Company in India</title>
+    <title>Best Packaging Company in India | Prem Industries India Limited</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
     <!-- Bootstrap core css -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
