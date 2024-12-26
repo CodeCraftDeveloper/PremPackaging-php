e0838d575ec74e92151e1f13ead2a25bb33378d1
unknown-dev-257 <ecommerce@premindustries.in>
2024-12-26 15:06:24 +0530

    updated paper mailer bags

--- paper-mailer-bags.php
+++ paper-mailer-bags.php
@@ -44,50 +44,57 @@
       </div>
     </div>
 
-    <section class="body__content padding page-body-content">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="text-center mb-4">
-              <h3 class="wow fadeInUp">Redefining Secure & Eco-friendly  Packaging</h3>
-            </div>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-md-10 offset-md-1">
-            <div class="body-content text-center">
-              <p class="wow fadeInUp">Get an innovative mix of paper and the strength and flexibility of plastic in mailer bags. We manufacture different sizes of paper mailer bags that are used by courier, parcel & freight firms for online order delivery, secure mailing, storage of sensitive data & the secure movement of their client's documents and consignments. Our mailer bag has Peel-&-seal strips for easy closure and reuses, open along the perforated line, remove the strip and reseal if you wish to reuse it.</p>
-      
-            </div>
+    <section class="body__content mt-5 page-body-content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-left mb-4">
+            <h3 class="wow fadeInUp">Paper Mailer Bag - Making life simpler</h3>
           </div>
         </div>
       </div>
-    </section>
+      <div class="row mt-3">
+        <div class="col-md-10 order2 mb-3">
+          <div class="body-content">
+            <p class="wow fadeInUp">Paper mailer bags are a sustainable alternative to traditional plastic mailers, designed to offer secure and durable packaging solutions. Made from high-quality paper materials, they provide strength and flexibility, making them ideal for a variety of applications such as courier services, parcel deliveries, and document storage.  </p>
+            <h4 style="font-weight: 600;">Why Choose our Paper Mailer Bags?</h4>
+            <p class="wow fadeInUp">At Prem Industries India Limited, we understand the importance of packaging that combines functionality, aesthetics, and sustainability. Our paper mailer bags are designed with features like peel-and-seal strips for secure closures and perforated lines for easy opening and reusability. Whether you are looking to deliver online orders, securely mail sensitive documents, or store confidential data, our range of paper mailer bags is equipped to handle it all.</p>
+            <h4 style="font-weight: 600;">Redefining Secure & Eco-friendly Packaging</h4>
+            <p>By opting for paper mailer bags from us, you get an innovative blend of durability and eco-friendliness. These bags are available in various sizes to cater to diverse business requirements and offer a sustainable way to reduce your carbon footprint. Trust us to deliver the best quality paper mailer bags, ensuring your packaging solutions are as secure and reliable.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+      </div>
+    </div>
+</section>
 
-    <div class="e-commerce-gallery BoxesGallery">
+<div class="e-commerce-gallery BoxesGallery">
       <div class="container-fluid">
-        <div class="row justify-content-center">
-          <div class="col-md-8 p-1">
+        <div class="row">
+          <div class="col-md-6 p-1">
             <a class="gallery-col" href="img/brown-paper-envelope-isolated.jpg" data-fancybox="images">
               <div class="gallery-box">
                 <img src="img/brown-paper-envelope-isolated.jpg" alt="">
-                <h4></h4>
+                <h4>Flipkart Paper Bags</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6 p-1">
+            <a class="gallery-col" href="img/amazon-paper-bag.jpg" data-fancybox="images">
+              <div class="gallery-box">
+                <img src="img/amazon-paper-bag.jpg" alt="">
+                <h4>Amazon Paper Bags</h4>
               </div>
             </a>
           </div>
         </div>
       </div>
-    </div>
+</div>
 
-    <section class="ContactPrem pt-0 mt-4">
-      <div class="container">
-        <div class="ContactPrem-content">
-          <h3>Connect to know us better!</h3>
-          <p>Let’s discuss a blend of your products and our packaging.</p>
-          <a href="contact-us.php" class="more-btn mt-4"><i class="fa fa-clone" aria-hidden="true"></i> Contact Us</a>
-        </div>
-      </div>
-    </section>
+<?php include 'contactsection.php';?>
+<!-- #region -->
   
   <!-- header start -->
   <?php include 'footer.php';?>
