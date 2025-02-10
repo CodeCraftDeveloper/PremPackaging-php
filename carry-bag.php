6f015ebd98ea203c422ce2140d6df99e3fe1ddd6
unknown-dev-257 <ecommerce@premindustries.in>
2025-02-10 14:49:05 +0530

    new images in carry bag

--- carry-bag.php
+++ carry-bag.php
@@ -94,66 +94,66 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/1-new.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/1.jpg" data-fancybox="images">
               <div class="gallery-box" class="fixed-bottom">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">W-Bottom Carry Bags without handle</h4>
-                <img src="img/carry-bag/1-new.jpg" alt="W-Bottom Carry Bags without handle">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">square bottom carry bag</h4>
+                <img src="img/carry-bag/new/1.jpg" alt="square bottom carry bag">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/2.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/2.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">W-Bottom Carry Bags with handle</h4>
-                <img src="img/carry-bag/2.jpg" alt="W-Bottom Carry Bags with handle">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">Luxury Carry Bag</h4>
+                <img src="img/carry-bag/new/2.jpg" alt="Luxury Carry Bag">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/3.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/3.jpg" data-fancybox="images">
               <div class="gallery-box">
-                <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bags with handle </h4>
-                <img src="img/carry-bag/3.jpg" alt="V-Bottom Carry Bags with handle ">
+                <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bag without handle </h4>
+                <img src="img/carry-bag/new/3.jpg" alt="V-Bottom Carry Bag without handle ">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/4.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/4.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bags with twisted handle</h4>
-                <img src="img/carry-bag/4.jpg" alt="V-Bottom Carry Bags with handle">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bag with twisted handle</h4>
+                <img src="img/carry-bag/new/4.jpg" alt="V-Bottom Carry Bag with handle">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/5.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/5.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bags without handle</h4>
-                <img src="img/carry-bag/5.jpg" alt="V-Bottom Carry Bags without handle">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">V-Bottom Carry Bag with handle</h4>
+                <img src="img/carry-bag/new/5.jpg" alt="V-Bottom Carry Bag with handle">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/6.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/6.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 style="align-items: flex-end; font-size: 16px;">Luxury Carry Bags</h4>
-                <img src="img/carry-bag/6.jpg" alt="Luxury Carry Bags">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">W-Bottom Carry Bag with handle</h4>
+                <img src="img/carry-bag/new/6.jpg" alt="W-Bottom Carry Bag with handle">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/7.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/7.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">square bottom carry bags </h4>
-                <img src="img/carry-bag/7.jpg" alt="square bottom carry bags">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">W-Bottom Carry Bag without handle</h4>
+                <img src="img/carry-bag/new/7.jpg" alt="W-Bottom Carry Bag without handle">
               </div>
             </a>
           </div>
           <div class="col-md-3 p-1">
-            <a class="gallery-col" href="img/carry-bag/8.jpg" data-fancybox="images">
+            <a class="gallery-col" href="img/carry-bag/new/8.jpg" data-fancybox="images">
               <div class="gallery-box">
-                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">D-cut carry bags</h4>
-                <img src="img/carry-bag/8.jpg" alt="D-cut carry bags">
+                  <h4 class="text-center" style="align-items: flex-end; font-size: 16px;">D-cut carry bag</h4>
+                <img src="img/carry-bag/new/8.jpg" alt="D-cut carry bag">
               </div>
             </a>
           </div>
