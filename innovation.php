6e3b2234e9c0715fc1afb0cb563d526bc8cca84b
unknown-dev-257 <ecommerce@premindustries.in>
2025-02-28 17:31:54 +0530

    innovation page images & links

--- innovation.php
+++ innovation.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Experience groundbreaking packaging innovation at Prem Industries India Limited. Discover innovative solutions tailored to your needs. Partner with us for a packaging revolution!">
-    <meta property="og:title" content="Innovation in every packaging product at Prem Industries India limited ">
+    <meta property="og:title" content="Innovation in every packaging product | Prem Industries India limited">
     <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
@@ -26,13 +26,12 @@
   <body>
     <!-- header start -->
     <?php include 'header.php';?>
-
-
+    <!-- header end -->
     <div class="page-hero ino-scan">
       <div class="container">
         <div class="row">
           <div class="col-md-8">
-            <h1>Innovative Packaging </h1>
+            <h1>Innovative Packaging</h1>
             <h3 class="text-white">Research & Development Makes It Happen</h3>
           </div>
           <div class="col-md-12">
@@ -84,7 +83,7 @@
         <div class="col-lg-3 col-md-3">
           <div class="innovation_content">
             <div class="icon_inovations">
-              <img src="img/ino1.svg" alt="" width="100">
+              <img src="img/ino1.svg" alt="Material Technology" width="100">
             </div>
             <h3>Material Technology</h3>
             <p>Our material experts are revolutionizing the packaging process, developing fresh boxes, trays, cartons, pouches, paperboard and containerboard formulations, and developing novel materials for all of our substrates, including the substances, adhesives, and films necessary for efficient packaging.</p>
@@ -93,7 +92,7 @@
         <div class="col-lg-3 col-md-3">
           <div class="innovation_content">
             <div class="icon_inovations">
-              <img src="img/ino2.svg" alt="" width="100">
+              <img src="img/ino2.svg" alt="Packaging Design" width="100">
             </div>
             <h3>Packaging Design</h3>
             <p>As we pilot innovative packaging solutions with our customers, our team of designers gets concentrated on the appearance, feel, and functionality of our packaging.</p>
@@ -102,7 +101,7 @@
         <div class="col-lg-3 col-md-3">
           <div class="innovation_content">
             <div class="icon_inovations">
-              <img src="img/ino3.svg" alt="" width="100">
+              <img src="img/ino3.svg" alt="Machinery & Automation" width="100">
             </div>
             <h3>Machinery & Automation</h3>
             <p>Our machinery and automation team always remains on the front edge of automation design and engineering machinery that makes it possible for clients to adopt products, optimize their processes, cut costs, and achieve scaled efficiency.</p>
@@ -111,7 +110,7 @@
         <div class="col-lg-3 col-md-3">
           <div class="innovation_content">
             <div class="icon_inovations">
-              <img src="img/ino4.svg" alt="" width="100">
+              <img src="img/ino4.svg" alt="Digital Excellence" width="100">
             </div>
             <h3>Digital Excellence</h3>
             <p>To improve processes while transforming the customer experience, our digital technologists build connected solutions that seamlessly combine our physical products and digital capabilities.</p>
@@ -127,80 +126,72 @@
     </div>
   </div>
 </div>
-  <div class="e-commerce-gallery BoxesGallery">
-      <div class="container-fluid">
+<div class="e-commerce-gallery">
+      <div class="container">
         <div class="row">
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/wetbox-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/wetbox-img.png" alt="">
-                <h4>WetBoxTech™</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+            <a href="https://prempackaging.com/wetboxtech" style="color: black;" target="_blank">
+              <img src="img/innovation/wetbox-img.png" alt="WetBoxTech™">
+              <h4 class="text-center">WetBoxTech™</h4>
+              </a>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/flexpand-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/flexpand-img.png" alt="">
-                <h4>Flexpand™</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+              <a href="https://prempackaging.com/flexpand" style="color: black;" target="_blank">
+              <img src="img/innovation/flex.png" alt="Flexpand™">
+              <h4 class="text-center">Flexpand™</h4>
+              </a>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/labels-with-multi-ups.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/labels-with-multi-ups.png" alt="">
-                <h4>Labels with Multi-ups</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+            <a href="https://prempackaging.com/labels-with-multiups" style="color: black;" target="_blank">
+              <img src="img/innovation/labels-with-multi-ups.png" alt="Labels with Multi-ups">
+              <h4 class="text-center">Labels with Multi-ups</h4>
+              </a>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/index/whats-new/apeo-free-tape-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/index/whats-new/apeo-free-tape-img.png" alt="">
-                <h4>APEO Free Tape</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+              <img src="img/innovation/apeo.png" alt="APEO Free Tape">
+              <h4 class="text-center">APEO Free Tape</h4>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/aluminium-tape-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/aluminium-tape-img.png" alt="">
-                <h4>Aluminium Tape</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+              <img src="img/innovation/alu.png" alt="Aluminium Tape">
+              <h4 class="text-center">Aluminium Tape</h4>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/masking-tape-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/masking-tape-img.png" alt="">
-                <h4>Masking Tape</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+              <img src="img/innovation/masking-tape-img.png" alt="Masking Tape">
+              <h4 class="text-center">Masking Tape</h4>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/glass-cloth-tape.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/glass-cloth-tape.png" alt="">
-                <h4>Glass Cloth Tape</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+              <img src="img/innovation/heattech-box.png" alt="HeatTechBox">
+              <h4 class="text-center">HeatTechBox</h4>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/innovation/honeycomb-wrapper-img.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/innovation/honeycomb-wrapper-img.png" alt="">
-                <h4>Honeycomb Wrapper</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+            <a href="https://prempackaging.com/packpro-honeycomb-food-wrapping-paper" style="color: black;" target="_blank">
+              <img src="img/innovation/honeycomb-wrapper-img.png" alt="Honeycomb Wrapper">
+              <h4 class="text-center">Honeycomb Wrapper</h4>
+              </a>
+            </div>
           </div>
-          <div class="col-md-4 p-1">
-            <a class="gallery-col" href="img/carry-handel-tape/image4.png" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/carry-handel-tape/image4.png" alt="">
-                <h4>Carry Handel Tape</h4>
-              </div>
-            </a>
+          <div class="col-md-4">
+            <div class="gallery-box">
+            <a href="https://prempackaging.com/carry-handle-tape" style="color: black;" target="_blank">
+              <img src="img/carry-handel-tape/image4.png" alt="PackPro™ Carry Handel Tape">
+              <h4 class="text-center">PackPro™ Carry Handel Tape</h4>
+              </a>
+            </div>
           </div>
         </div>
       </div>
