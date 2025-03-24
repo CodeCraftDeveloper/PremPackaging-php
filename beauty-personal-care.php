d7ff206f94af7a6964f15d2cff62cab47aca8c0d
unknown-dev-257 <ecommerce@premindustries.in>
2025-03-24 14:15:17 +0530

    updated redirect logic

--- beauty-personal-care.php
+++ beauty-personal-care.php
@@ -1,5 +1,5 @@
 <?php
   // Redirect to the new URL with a 301 status code
-header('Location: /beauty-personal-care-packaging.php', true, 301);
+header('Location: /beauty-personal-care-packaging', true, 301);
 exit;
 ?>
--- carry-handle-tape.php
+++ carry-handle-tape.php
@@ -1,5 +1,5 @@
 <?php
   // Redirect to the new URL with a 301 status code
-header('Location: /packpro-carry-handle-tape.php', true, 301);
+header('Location: /packpro-carry-handle-tape', true, 301);
 exit;
 ?>
