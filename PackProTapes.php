43a5dac0ffb33220cc2499752e7d50d246aafb1c
unknown-dev-257 <ecommerce@premindustries.in>
2025-03-26 12:01:08 +0530

    Rollabel & Packpro Slug Blocking

---
+++ PackProTapes.php
@@ -0,0 +1,5 @@
+<?php
+  // Redirect to the new URL with a 301 status code
+header('Location: /packpro-tapes', true, 301);
+exit;
+?>
---
+++ rollabel.php
@@ -0,0 +1,5 @@
+<?php
+  // Redirect to the new URL with a 301 status code
+header('Location: /rollabel-labels', true, 301);
+exit;
+?>
