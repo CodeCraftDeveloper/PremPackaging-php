7f2f63490d486b9ec7f6e80bd73fd7efa214e218
unknown-dev-257 <ecommerce@premindustries.in>
2025-04-25 10:59:16 +0530

    Updated Page

--- beauty-personal-care.php
+++
@@ -1,5 +0,0 @@
-<?php
-  // Redirect to the new URL with a 301 status code
-header('Location: /beauty-personal-care-packaging', true, 301);
-exit;
-?>
--- carry-handle-tape.php
+++
@@ -1,5 +0,0 @@
-<?php
-  // Redirect to the new URL with a 301 status code
-header('Location: /packpro-carry-handle-tape', true, 301);
-exit;
-?>
