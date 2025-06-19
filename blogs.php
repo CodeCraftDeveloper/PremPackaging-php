155b9879a260509cd808a7a2fbbd2d825f7d23b1
unknown-dev-257 <ecommerce@premindustries.in>
2025-06-19 12:16:45 +0530

    Test

--- blogs.php
+++ blogs.php
@@ -8,8 +8,7 @@
     content="Get the latest updates, news, and insights on packaging. Stay informed about trends, innovations, and other industry details.">
   <meta property="og:title" content="Packaging Blogs | Prem Industries India Limited">
   <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96">
-  </script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag() {
@@ -18,9 +17,7 @@
     gtag('js', new Date());
     gtag('config', 'G-PSMKSG4F96');
   </script>
-  <title>
-    Packaging Blogs | Prem Industries India Limited
-  </title>
+  <title>Packaging Blogs | Prem Industries India Limited</title>
   <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
@@ -122,6 +119,28 @@
       <div class="row">
         <div class="col-md-7 pb-5">
           <div class="row row-cols-1 row-cols-md-2 g-4">
+            <!-- Blog 6 -->
+            <div class="col mt-4 p-0 pr-1" data-aos="fade-up" data-aos-delay="100">
+              <div class="card h-100 d-flex flex-column shadow-sm">
+                <a href="how-to-choose-the-right-carry-bag-manufacturer-for-your-business.php"
+                  style="text-decoration: none; color: black;">
+                  <img src="img/blog/cards/how-to-choose-the-right-carry-bag-manufacturer-for-your-business.jpg"
+                    class="card-img-top" alt="How to Choose the Right Carry Bag Manufacturer for Your Business">
+                  <div class="card-body flex-grow-1">
+                    <b style="font-size: 18px;">
+                      How to Choose the Right Carry Bag Manufacturer for Your Business
+                    </b>
+                    <br>
+                    <small class="text-muted">Last Updated: <b>19th June 2025</b></small>
+                    <p class="card-text mt-2">When it comes to packaging, carry bags are more than just a
+                      convenience—they’re an extension of your brand... <b class="text-danger">Read&nbsp;More...</b></p>
+                  </div>
+                </a>
+                <!-- <div class="card-footer mt-auto">
+                    <small class="text-muted">23rd May 2025</small>
+                  </div> -->
+              </div>
+            </div>
             <!-- Blog 5 -->
             <div class="col mt-4 p-0 pr-1" data-aos="fade-up" data-aos-delay="100">
               <div class="card h-100 d-flex flex-column shadow-sm">
@@ -241,12 +260,32 @@
             </div>
           </div>
         </div>
-        <div class="col-md-5 pt-4 mt-2 sticky-top" data-aos="fade-up">
-          <div class="container-fluid rounded shadow-lg" style="background-color: #182C5A;">
+        <div class="col-md-5 pt-4 mt-2" data-aos="fade-up">
+          <div class="container-fluid rounded shadow-lg sticky-top" style="background-color: #182C5A;">
             <h3 class="text-center pt-4" style="font-weight: bold; text-transform: uppercase; color: white;">Popular
               Blogs
             </h3>
             <div class="row pt-3 pb-3">
+
+              <div class="col-md-12">
+                <a href="how-to-choose-the-right-carry-bag-manufacturer-for-your-business.php"
+                  style="text-decoration: none; color: black;">
+                  <div class="card mb-3 shadow-sm" style="max-width: 540px;">
+                    <div class="row g-0">
+                      <div class="col-md-4 d-flex align-items-center justify-content-center">
+                        <img src="img/blog/popular/06.jpg" class="img-fluid rounded h-100 w-100 p-1"
+                          alt="How to Choose the Right Carry Bag Manufacturer for Your Business">
+                      </div>
+                      <div class="col-md-8 d-flex align-items-center">
+                        <div class="card-body p-0 w-100">
+                          <h5 class="card-title m-0 p-2" style="font-size: 15px;">How to Choose the Right Carry Bag
+                            Manufacturer for Your Business</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
 
               <div class="col-md-12">
                 <a href="how-to-find-a-trustworthy-packaging-manufacturer-without-breaking-the-bank.php"
@@ -254,9 +293,7 @@
                   <div class="card mb-3 shadow-sm" style="max-width: 540px;">
                     <div class="row g-0">
                       <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <img
-                          src="img/blog/cards/how-to-find-a-trustworthy-packaging-manufacturer-without-breaking-the-bank.jpg"
-                          class="img-fluid rounded h-100 w-100 p-1"
+                        <img src="img/blog/popular/01.jpg" class="img-fluid rounded h-100 w-100 p-1"
                           alt="How to find a Trustworthy Packaging Manufacturer Without Breaking the Bank">
                       </div>
                       <div class="col-md-8 d-flex align-items-center">
@@ -276,9 +313,7 @@
                   <div class="card mb-3 shadow-sm" style="max-width: 540px;">
                     <div class="row g-0">
                       <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <img
-                          src="img/blog/cards/lift-with-ease-how-packpro-carry-handle-tape-transforms-heavy-packaging.jpg"
-                          class="img-fluid rounded h-100 w-100 p-1"
+                        <img src="img/blog/popular/02.jpg" class="img-fluid rounded h-100 w-100 p-1"
                           alt="Lift with Ease: How PackPro™ Carry Handle Tape Transforms Heavy Packaging">
                       </div>
                       <div class="col-md-8 d-flex align-items-center">
@@ -298,8 +333,7 @@
                   <div class="card mb-3 shadow-sm" style="max-width: 540px;">
                     <div class="row g-0">
                       <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <img src="img/blog/cards/why-healthcare-packaging-is-crucial-for-safety-and-hygienee.jpg"
-                          class="img-fluid rounded h-100 w-100 p-1"
+                        <img src="img/blog/popular/03.jpg" class="img-fluid rounded h-100 w-100 p-1"
                           alt="Why Healthcare Packaging is Crucial for Safety and Hygiene">
                       </div>
                       <div class="col-md-8 d-flex align-items-center">
@@ -319,8 +353,7 @@
                   <div class="card mb-3 shadow-sm" style="max-width: 540px;">
                     <div class="row g-0">
                       <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <img src="img/blog/cards/types-of-pouches-we-provide-at-prem-industries-india-limited.png"
-                          class="img-fluid rounded h-100 w-100 p-1"
+                        <img src="img/blog/popular/05.jpg" class="img-fluid rounded h-100 w-100 p-1"
                           alt="Types of Pouches We Provide at Prem Industries India Limited">
                       </div>
                       <div class="col-md-8 d-flex align-items-center">
@@ -340,8 +373,7 @@
                   <div class="card shadow-sm" style="max-width: 540px;">
                     <div class="row g-0">
                       <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <img src="img/blog/cards/how-to-choose-the-right-bespoke-packaging-supplier.png"
-                          class="img-fluid rounded h-100 w-100 p-1"
+                        <img src="img/blog/popular/04.jpg" class="img-fluid rounded h-100 w-100 p-1"
                           alt="How to Choose the Right Bespoke Packaging Supplier">
                       </div>
                       <div class="col-md-8 d-flex align-items-center">
