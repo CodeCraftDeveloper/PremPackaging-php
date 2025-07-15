ef26f59f769c9ed971217854b68f58c57458e9f1
unknown-dev-257 <ecommerce@premindustries.in>
2025-07-15 11:26:48 +0530

    Luxury Packaging Content Change

--- luxury-packaging.php
+++ luxury-packaging.php
@@ -1,48 +1,62 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Discover our exquisite luxury packaging solutions, crafted with premium materials and impeccable designs for a truly exceptional experience.">
-    <meta property="og:title" content="Luxury Packaging Manufacturer | Prem Industries India Limited">
-    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>Luxury Packaging Manufacturer</title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
-    <!-- Bootstrap core css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
-    <!-- Custom styles for this site -->
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
-  </head>
-  <body>
-    <!-- header start -->
-    <?php include 'header.php';?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Discover our exquisite luxury packaging products, crafted by a leading luxury packaging manufacturer using premium materials and impeccable designs for a truly exceptional experience.">
+  <meta property="og:title" content="Luxury Packaging Manufacturer | Prem Industries India Limited">
+  <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
+  <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
+
+  <title>Luxury Packaging Manufacturer | Prem Industries India Limited</title>
+  <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+  <!-- Bootstrap core css -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
+  <!-- Custom styles for this site -->
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/media.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <style>
+    h4 {
+      color: #14254C;
+    }
+
+    .bullet-point-new {
+      color: #14254C;
+    }
+
+    b {
+      color: #14254C;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- header start -->
+  <?php include 'header.php'; ?>
 
 
-    <div class="page-hero pageheade luxury-pack-b">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <h1>LUXURY PACKAGING</h1>
-          </div>
-          <div class="col-md-12">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Luxury Packaging</li>
-              </ol>
-            </nav>
-          </div>
+  <div class="page-hero pageheade luxury-pack-b">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>LUXURY PACKAGING</h1>
+        </div>
+        <div class="col-md-12">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Luxury Packaging</li>
+            </ol>
+          </nav>
         </div>
       </div>
+    </div>
   </div>
 
   <section class="body__content mt-5 page-body-content">
@@ -55,27 +69,49 @@
         </div>
       </div>
       <div class="row mt-3">
-        <div class="col-md-6 order2 mb-3">
+        <div class="col-md-6 order2 mb-3" style="text-align: justify">
           <div class="body-content">
-            <p class="wow fadeInUp">Premium products need packaging that reflects the same care and attention to detail that they are made with. When a consumer buys into a luxury brand they want to be pampered from the moment they get the packaging. Our luxury packaging division creates solutions that ensure this by using the finest materials and creating designs that reflect the brand’s personality. Together this work creates an impression that whispers sophistication.</p>
+            <p class="wow fadeInUp">Premium products need packaging that reflects the same care and attention to detail
+              that they are made with. When a consumer buys into a luxury brand they want to be pampered from the moment
+              they get the packaging. As a trusted luxury packaging manufacturer, our luxury packaging division creates
+              solutions that ensure this by using the finest materials and crafting designs that reflect the brand’s
+              personality. Together this work creates an impression that whispers sophistication.</p>
             <h4 style="font-weight: 600;">Creative Expertise That Builds Your Brand</h4>
-            <p class="wow fadeInUp">Our research and development team stays connected to the market trends that can appeal to the consumer. Technicians make sure quality-assured packaging that remains above the industry's standards of manufacturing practices.
-            <br>Our industry experts offer imaginative and functional designs that build your brand equity and market presence: </p>
+            <p class="wow fadeInUp">Our research and development team stays connected to the market trends that can
+              appeal to the consumer. Technicians make sure quality-assured packaging that remains above the industry's
+              standards of manufacturing practices.
+              <br>Our industry experts offer imaginative and functional designs that build your brand equity and market
+              presence:
+            </p>
             <ul class="mb-3">
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Ideation & Conceptualization</li>
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Packaging enhancement</li>
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> 3D rendering capabilities and prototype development</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Facilitation of line trial sampling</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Specifications management</li> 
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span
+                  class="bullet-point-new">⦿</span></i>
+                Ideation & Conceptualization</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span
+                  class="bullet-point-new">⦿</span></i>
+                Packaging enhancement</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span
+                  class="bullet-point-new">⦿</span></i> 3D
+                rendering capabilities and prototype development</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span
+                  class="bullet-point-new">⦿</span></i>
+                Facilitation of line trial sampling</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span
+                  class="bullet-point-new">⦿</span></i>
+                Specifications management</li>
             </ul>
-            <p class="wow fadeInUp">As a leading manufacturer of luxury packaging, we offer a unique breadth of capabilities for all brands. Creating solutions for all products and brands as needed.</p>
+            <p class="wow fadeInUp">As a leading luxury packaging manufacturer, we offer a unique breadth of
+              capabilities tailored to your brand’s vision. Creating solutions for all products and brands as needed.
+            </p>
             <h4 style="font-weight: 600;">Are you Ready to Elevate Your luxury Packaging?</h4>
-            <p class="wow fadeInUp">Contact us today to transform your packaging into a luxurious experience that captivates your customers and amplifies your brand!</p>
+            <p class="wow fadeInUp">Contact us today to transform your packaging into a luxurious experience that
+              captivates your customers and amplifies your brand!</p>
           </div>
         </div>
         <div class="col-md-6 order1">
           <div class="design-image wow" data-wow-delay="0.5s">
-            <img class="image1" width="665" height="870" src="img/luxury-packaging/luxury-1.jpg" alt="Luxury Packaging Solutions" />
+            <img class="image1" width="665" height="870" src="img/luxury-packaging/luxury-1.jpg"
+              alt="Luxury Packaging Solutions" />
           </div>
         </div>
       </div>
@@ -89,13 +125,13 @@
     <h4 class="bold-title">We guard and enhance the image of your brand with the best packaging possible.</h4>
   </div>
 
-  <?php include 'contactsection.php';?>
+  <?php include 'contactsection.php'; ?>
 
-  
+
   <!-- header start -->
-  <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
 
-  
+
   <script type="text/javascript">
     $(document).ready(function () {
       $('.fancybox').fancybox();
