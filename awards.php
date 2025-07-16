73434dced7738bb1800e4381640675b251297c26
unknown-dev-257 <ecommerce@premindustries.in>
2025-07-16 13:09:19 +0530

    Quality Page Certificate

--- awards.php
+++ awards.php
@@ -1,169 +1,211 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Awards - Prem Packgaing">
-    <meta property="og:title" content="Prem Packaging">
-    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-PSMKSG4F96');
-    </script>
 
-    <title>Awards | Prem Industries India Limited</title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
-    <!-- Bootstrap core css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
-    <!-- Custom styles for this site -->
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
-    <style>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Awards - Prem Packaging">
+  <meta property="og:title" content="Prem Packaging">
+  <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-PSMKSG4F96');
+  </script>
+
+  <title>Awards | Prem Industries India Limited</title>
+  <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/media.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+  <style>
+    .page-hero {
+      height: 350px;
+      background-size: cover;
+      background-position: center;
+    }
+
+    @media (max-width: 460px) {
       .page-hero {
-        height: 350px;
-        background-size: cover;
-        background-position: center;
+        background-image: url('./img/awards/awards-banner.jpg');
       }
-      @media (max-width: 460px) {
-        .page-hero {
-          background-image: url('./img/awards/awards-banner.jpg');
-        }
+    }
+
+    @media (min-width: 30000px) {
+      .page-hero {
+        background-image: url('https://placehold.co/1500x350');
       }
-      @media (min-width: 30000px) {
-        .page-hero {
-          background-image: url('https://placehold.co/1500x350');
-        }
+    }
+
+    @media (min-width: 461px) and (max-width: 20000px) {
+      .page-hero {
+        background-image: url('./img/awards/awards-banner.jpg');
       }
-      @media (min-width: 461px) and (max-width: 20000px) {
-        .page-hero {
-          background-image: url('./img/awards/awards-banner.jpg');
-        }
-      }
-    </style>
-  </head>
-  <body>
-    <!-- header start -->
-    <?php include 'header.php';?>
+    }
 
+    #pdfModal {
+      display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #000000cc;
+      z-index: 9999;
+    }
 
-    <div class="page-hero pageheade exibit-banner">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <h1 class="banner-head">Awards</h1>
-          </div>
-          <div class="col-md-9">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Awards</li>
-              </ol>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </div>
+    #pdfViewerWrapper {
+      margin: 60px auto 0;
+      max-height: 85vh;
+      overflow: auto;
+      background: #fff;
+      padding: 20px;
+      width: 90%;
+    }
 
-    <section class="body__content mt-5 page-body-content">
-      <div class="container">
-        <div class="row mb-3">
-          <div class="col-md-12 text-center">
-            <h3 class="wow fadeInUp" style="font-size: 35px">Our Awards</h3>
-          </div>
-        </div>
-      </div>
-    </section>
+    #pdf-pages canvas {
+      display: block;
+      margin: 20px auto;
+    }
+  </style>
+</head>
 
-    <div class="e-commerce-gallery BoxesGallery mt-3">
-            <div class="container-fluid">
-              <div class="row">
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/1.png" data-fancybox="images">
-                    <div class="gallery-box" class="fixed-bottom">
-                        <h4 style="align-items: flex-end;">Symphony 2024</h4>
-                      <img src="img/awards/1.png" alt="Symphony Award 2024">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/2.png" data-fancybox="images">
-                    <div class="gallery-box">
-                        <h4 style="align-items: flex-end;">Haier 2022</h4>
-                      <img src="img/awards/2.png" alt="Gulfood Manufacturing Dubai">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/3.png" data-fancybox="images">
-                    <div class="gallery-box">
-                      <h4 style="align-items: flex-end;">Veeta Day 2024</h4>
-                      <img src="img/awards/3.png" alt="Gulfood Manufacturing Dubai">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/4.png" data-fancybox="images">
-                    <div class="gallery-box">
-                        <h4 style="align-items: flex-end;">Grofers 2019</h4>
-                      <img src="img/awards/4.png" alt="Gulfood Manufacturing Dubai">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/5.png" data-fancybox="images">
-                    <div class="gallery-box">
-                        <h4 style="align-items: flex-end;">IAMA 2019</h4>
-                      <img src="img/awards/5.png" alt="AAHAR 2024">
-                    </div>
-                  </a>
-                </div>
-                <div class="col-md-4 p-1">
-                  <a class="gallery-col" href="img/awards/6.png" data-fancybox="images">
-                    <div class="gallery-box">
-                        <h4 style="align-items: flex-end;">Good Morning 2024</h4>
-                      <img src="img/awards/6.png" alt="AAHAR 2024">
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+<body>
+  <?php include 'header.php'; ?>
 
-    <section class="body__content page-body-content">
-      <div class="container">
-        <div class="row mb-3">
-          <div class="col-md-12 text-center">
-            <h3 class="wow fadeInUp" style="font-size: 35px">EPR CERTIFICATE</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-
+  <div class="page-hero pageheade exibit-banner">
     <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-4 text-center mb-5">
-          <img src="certificate/EPR.png" alt="EPR Certificate" class="img-fluid" />
+      <div class="row">
+        <div class="col-md-8">
+          <h1 class="banner-head">Awards</h1>
+        </div>
+        <div class="col-md-9">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Awards</li>
+            </ol>
+          </nav>
         </div>
       </div>
     </div>
+  </div>
 
-    <?php include 'contactsection.php';?>
-    <!-- header start -->
-    <?php include 'footer.php';?>
+  <section class="body__content mt-5 page-body-content">
+    <div class="container">
+      <div class="row mb-3">
+        <div class="col-md-12 text-center">
+          <h3 class="wow fadeInUp" style="font-size: 35px">Our Awards</h3>
+        </div>
+      </div>
+    </div>
+  </section>
 
-    <script type="text/javascript">
-      $(document).ready(function () {
-        $('.fancybox').fancybox();
+  <div class="e-commerce-gallery BoxesGallery mt-3">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- Award Images -->
+        <?php
+        $awards = [
+          ["1.png", "Symphony 2024"],
+          ["2.png", "Haier 2022"],
+          ["3.png", "Veeta Day 2024"],
+          ["4.png", "Grofers 2019"],
+          ["5.png", "IAMA 2019"],
+          ["6.png", "Good Morning 2024"]
+        ];
+        foreach ($awards as $award) {
+          echo '<div class="col-md-4 p-1">
+            <a class="gallery-col" href="img/awards/' . $award[0] . '" data-fancybox="images">
+              <div class="gallery-box">
+                <h4 style="align-items: flex-end;">' . $award[1] . '</h4>
+                <img src="img/awards/' . $award[0] . '" alt="' . $award[1] . '">
+              </div>
+            </a>
+          </div>';
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+
+  <section class="body__content page-body-content">
+    <div class="container">
+      <div class="row mb-3">
+        <div class="col-md-12 text-center">
+          <h3 class="wow fadeInUp" style="font-size: 35px">EPR CERTIFICATE</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- PDF Trigger Section -->
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-4 text-center mb-5">
+        <a href="javascript:void(0);" onclick="showPDF()">
+          <img src="certificate/EPR.png" alt="EPR Certificate" class="img-fluid" />
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <!-- PDF Modal Viewer -->
+  <div id="pdfModal">
+    <div style="position:absolute; top:10px; right:20px; z-index:10000;">
+      <button onclick="closePDF()" style="padding:6px 12px; font-size:16px;">Close</button>
+    </div>
+    <div id="pdfViewerWrapper">
+      <div id="pdf-pages"></div>
+    </div>
+  </div>
+
+  <?php include 'contactsection.php'; ?>
+  <?php include 'footer.php'; ?>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js"></script>
+  <script>
+    function showPDF() {
+      const modal = document.getElementById('pdfModal');
+      const container = document.getElementById('pdf-pages');
+      const url = 'certificate/EPR.pdf';
+
+      modal.style.display = 'block';
+      document.body.style.overflow = 'hidden'; // Disable background scroll
+      container.innerHTML = ''; // Clear previous
+
+      pdfjsLib.getDocument(url).promise.then(function (pdf) {
+        const totalPages = pdf.numPages;
+
+        for (let pageNum = 1; pageNum <= totalPages; pageNum++) {
+          pdf.getPage(pageNum).then(function (page) {
+            const viewport = page.getViewport({ scale: 1.5 });
+            const canvas = document.createElement('canvas');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            container.appendChild(canvas);
+
+            const renderContext = {
+              canvasContext: canvas.getContext('2d'),
+              viewport: viewport
+            };
+            page.render(renderContext);
+          });
+        }
       });
-    </script>
-  </body>
+    }
+
+    function closePDF() {
+      document.getElementById('pdfModal').style.display = 'none';
+      document.body.style.overflow = ''; // Re-enable scroll
+    }
+  </script>
+</body>
+
 </html>
--- paper-mailer-bags.php
+++ paper-mailer-bags.php
@@ -1,58 +1,60 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Discover eco-friendly paper mailer bags for secure shipping. Enhance your packaging with sustainable solutions. Order now for eco-conscious shipping.">
-    <meta property="og:title" content="Buy Paper Mailer Bags| Prem Industries India Limited">
-    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>Paper Mailer Bags | Prem Industries India Limited</title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
-    <!-- Bootstrap core css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
-    <!-- Custom styles for this site -->
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
-    <style>
-      h4{
-        color: #14254C;
-      }
-      .bullet-point-new{
-        color: #14254C;
-      }
-    </style>
-  </head>
-  <body>
-    <!-- header start -->
-    <?php include 'header.php';?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Discover eco-friendly paper mailer bags for secure shipping. Enhance your packaging with sustainable solutions. Order now for eco-conscious shipping.">
+  <meta property="og:title" content="Buy Paper Mailer Bags| Prem Industries India Limited">
+  <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
+  <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <div class="page-hero Paper-Mailer-Bags pageheade">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <h1>Paper Mailer Bags</h1>
-          </div>
-          <div class="col-md-12">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Paper Mailer Bags</li>
-              </ol>
-            </nav>
-          </div>
+  <title>Paper Mailer Bags | Prem Industries India Limited</title>
+  <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+  <!-- Bootstrap core css -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css" />
+  <!-- Custom styles for this site -->
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/media.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <style>
+    h4 {
+      color: #14254C;
+    }
+
+    .bullet-point-new {
+      color: #14254C;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- header start -->
+  <?php include 'header.php'; ?>
+
+  <div class="page-hero Paper-Mailer-Bags pageheade">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>Paper Mailer Bags</h1>
+        </div>
+        <div class="col-md-12">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#"><i class="fa fa-home"></i> Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Paper Mailer Bags</li>
+            </ol>
+          </nav>
         </div>
       </div>
     </div>
+  </div>
 
-    <section class="body__content mt-5 page-body-content">
+  <section class="body__content mt-5 page-body-content">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
@@ -64,48 +66,61 @@
       <div class="row">
         <div class="col-md-12 order2 mb-3" style="text-align: justify">
           <div class="body-content">
-            <p class="wow fadeInUp">Paper mailer bags are a sustainable alternative to traditional plastic mailers, designed to offer secure and durable packaging solutions. Made from high-quality paper materials, they provide strength and flexibility, making them ideal for a variety of applications such as courier services, parcel deliveries, and document storage.  </p>
+            <p class="wow fadeInUp">Paper mailer bags are a sustainable alternative to traditional plastic mailers,
+              designed to offer secure and durable packaging solutions. Made from high-quality paper materials, they
+              provide strength and flexibility, making them ideal for a variety of applications such as courier
+              services, parcel deliveries, and document storage. </p>
             <h4 style="font-weight: 600;">Why Choose our Paper Mailer Bags?</h4>
-            <p class="wow fadeInUp">At Prem Industries India Limited, we understand the importance of packaging that combines functionality, aesthetics, and sustainability. Being a trusted paper mailer bags manufacturer, we understand the importance of packaging that combines functionality, aesthetics, and sustainability. Our paper mailer bags are designed with features like peel-and-seal strips for secure closures and perforated lines for easy opening and reusability. Whether you are looking to deliver online orders, securely mail sensitive documents, or store confidential data, our range of paper mailer bags is equipped to handle it all.</p>
+            <p class="wow fadeInUp">At Prem Industries India Limited, we understand the importance of packaging that
+              combines functionality, aesthetics, and sustainability. Being a trusted paper mailer bags manufacturer, we
+              understand the importance of packaging that combines functionality, aesthetics, and sustainability. Our
+              paper mailer bags are designed with features like peel-and-seal strips for secure closures and perforated
+              lines for easy opening and reusability. Whether you are looking to deliver online orders, securely mail
+              sensitive documents, or store confidential data, our range of paper mailer bags is equipped to handle it
+              all.</p>
             <h4 style="font-weight: 600;">Redefining Secure & Eco-friendly Packaging</h4>
-            <p>By opting for paper mailer bags from us, you get an innovative blend of durability and eco-friendliness. These bags are available in various sizes to cater to diverse business requirements and offer a sustainable way to reduce your carbon footprint. Trust us to deliver the best quality paper mailer bags, ensuring your packaging solutions are as secure and reliable.</p>
+            <p>By opting for paper mailer bags from us, you get an innovative blend of durability and eco-friendliness.
+              These bags are available in various sizes to cater to diverse business requirements and offer a
+              sustainable way to reduce your carbon footprint. Trust us as your reliable paper mailer bags manufacturer
+              to deliver the best quality paper mailer bags, ensuring your packaging solutions are as secure and
+              reliable.</p>
           </div>
         </div>
       </div>
     </div>
+    </div>
+    </div>
+    </div>
+  </section>
+
+  <div class="e-commerce-gallery BoxesGallery">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 p-1">
+          <a class="gallery-col" href="img/brown-paper-envelope-isolated.jpg" data-fancybox="images">
+            <div class="gallery-box">
+              <img src="img/brown-paper-envelope-isolated.jpg" alt="">
+              <h4>Flipkart Paper Bags</h4>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-6 p-1">
+          <a class="gallery-col" href="img/amazon-paper-bag.jpg" data-fancybox="images">
+            <div class="gallery-box">
+              <img src="img/amazon-paper-bag.jpg" alt="">
+              <h4>Amazon Paper Bags</h4>
+            </div>
+          </a>
         </div>
       </div>
     </div>
-</section>
+  </div>
 
-<div class="e-commerce-gallery BoxesGallery">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-6 p-1">
-            <a class="gallery-col" href="img/brown-paper-envelope-isolated.jpg" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/brown-paper-envelope-isolated.jpg" alt="">
-                <h4>Flipkart Paper Bags</h4>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6 p-1">
-            <a class="gallery-col" href="img/amazon-paper-bag.jpg" data-fancybox="images">
-              <div class="gallery-box">
-                <img src="img/amazon-paper-bag.jpg" alt="">
-                <h4>Amazon Paper Bags</h4>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-</div>
+  <?php include 'contactsection.php'; ?>
+  <!-- #region -->
 
-<?php include 'contactsection.php';?>
-<!-- #region -->
-  
   <!-- header start -->
-  <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
 
   <script type="text/javascript">
     $(document).ready(function () {
