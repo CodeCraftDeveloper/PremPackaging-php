f3a87aa2960f7d525ee40cb2dd242791972b5a83
unknown-dev-257 <ecommerce@premindustries.in>
2025-07-16 16:45:07 +0530

    New Banner: Quality Control

--- quality-control.php
+++ quality-control.php
@@ -1,32 +1,57 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Discover how Prem Industries India Limited maintains top-tier quality standards in packaging. Our rigorous QC processes ensure excellence in every product. Experience Excellence with Prem Industries Quality Control.">
-    <meta property="og:title" content="Quality control services at Prem Industries QC Standards">
-    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>Quality Control</title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
-    <!-- Bootstrap core css -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this site -->
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
-  </head>
-  <body>
-    <!-- header start -->
-    <?php include 'header.php';?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Discover how Prem Industries India Limited maintains top-tier quality standards in packaging. Our rigorous QC processes ensure excellence in every product. Experience Excellence with Prem Industries Quality Control.">
+  <meta property="og:title" content="Quality control services at Prem Industries QC Standards">
+  <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
+  <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <div class="page-hero Quality-Control">
+  <title>Quality Control</title>
+  <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+  <!-- Bootstrap core css -->
+  <link rel="stylesheet" href="css/owl.carousel.min.css" />
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this site -->
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/media.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <!-- Custom styles for banner (add inside <head> or main.css) -->
+  <style>
+    .qc-banner {
+      width: 100vw;
+      overflow: hidden;
+      margin-left: calc(-50vw + 50%);
+      margin-right: calc(-50vw + 50%);
+      position: relative;
+      text-align: center;
+    }
+
+    .qc-banner-img {
+      width: 100%;
+      height: auto;
+      display: block;
+      object-fit: contain;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- header start -->
+  <?php include 'header.php'; ?>
+
+  <!-- Full-width banner start -->
+  <div class="qc-banner">
+    <img src="img/quality-control/banner-new.jpg" alt="Quality Control Banner" class="qc-banner-img">
+  </div>
+  <!-- Full-width banner end -->
+
+  <!-- <div class="page-hero Quality-Control">
       <div class="container">
         <div class="row">
           <div class="col-md-8">
@@ -42,46 +67,62 @@
           </div>
         </div>
       </div>
-  </div>
+  </div> -->
+
 
   <section class="body__content page-body-content">
     <div class="container">
-    <section class="body__content mt-5 page-body-content">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="text-left mb-4">
-            <h3 class="wow fadeInUp">Our Standards Reflect It</h3>
-          </div>
-        </div>
-      </div>
-      <div class="row mt-3">
-        <div class="col-md-6 order2 mb-3">
-          <div class="body-content">
-            <p class="wow fadeInUp">We have created an efficient ecosystem to stay compliant and deliver the packaging solutions consistently to achieve customer satisfaction. We have cutting-edge equipment that complies with international standards and an internal setup for everything needed to make a range of packaging.</p>
-            <p class="wow fadeInUp">Our firm has received a number of certifications that guarantee uniform standards, show transparency for regulators, and specify what quality means to us.</p>
-            <h4 style="font-weight: 600;">Testing</h4>
-            <p class="wow fadeInUp">We have labs and quality testing machines /equipments with experts operating them. The corrugated packaging goes through a number of quality checks to assure the best unboxing experience that reaches you. We follow a set process to test a packaging:
-            <ul class="mb-3">
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Bursting Strength</li>
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Edge Crush</li>
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Water Resistance</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Paper Grammage and Thickness</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Cobb Sizing</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Puncture Resistance</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Scuff Resistance</li> 
-              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i> Box Compression</li> 
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-6 order1">
-            <div class="design-image wow" data-wow-delay="0.5s">
-              <img class="image1" width="665" height="870" src="img/quality-control/research.jpg" alt="E-Commerce Packaging" title="E-Commerce Packaging" />
+      <section class="body__content mt-5 page-body-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="text-left mb-4">
+                <h3 class="wow fadeInUp">Our Standards Reflect It</h3>
+              </div>
             </div>
           </div>
-      </div>
-    </div>
-  </section>
+          <div class="row mt-3">
+            <div class="col-md-6 order2 mb-3">
+              <div class="body-content">
+                <p class="wow fadeInUp">We have created an efficient ecosystem to stay compliant and deliver the
+                  packaging solutions consistently to achieve customer satisfaction. We have cutting-edge equipment that
+                  complies with international standards and an internal setup for everything needed to make a range of
+                  packaging.</p>
+                <p class="wow fadeInUp">Our firm has received a number of certifications that guarantee uniform
+                  standards, show transparency for regulators, and specify what quality means to us.</p>
+                <h4 style="font-weight: 600;">Testing</h4>
+                <p class="wow fadeInUp">We have labs and quality testing machines /equipments with experts operating
+                  them. The corrugated packaging goes through a number of quality checks to assure the best unboxing
+                  experience that reaches you. We follow a set process to test a packaging:
+                <ul class="mb-3">
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Bursting Strength</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Edge Crush</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Water Resistance</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Paper Grammage and Thickness</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Cobb Sizing</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Puncture Resistance</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Scuff Resistance</li>
+                  <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><i class="fa fa-arrow-right"></i>
+                    Box Compression</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-6 order1">
+              <div class="design-image wow" data-wow-delay="0.5s">
+                <img class="image1" width="665" height="870" src="img/quality-control/research.jpg"
+                  alt="E-Commerce Packaging" title="E-Commerce Packaging" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <div class="row mt-5">
         <div class="col-md-12">
@@ -89,7 +130,8 @@
             <div class="col-lg-8 offset-lg-2">
               <div class="text-center mb-0 pb-0">
                 <h3 class="wow fadeInUp">Certifications</h3>
-                <p class="wow fadeInUp">We have received certifications from credible quality management authorities to prove our authentic presence in this industry.</p>
+                <p class="wow fadeInUp">We have received certifications from credible quality management authorities to
+                  prove our authentic presence in this industry.</p>
               </div>
             </div>
           </div>
@@ -155,22 +197,22 @@
     <h4 class="bold-title"></h4>
   </div>
 
-  <?php include 'contactsection.php';?>
+  <?php include 'contactsection.php'; ?>
 
-  
+
   <!-- header start -->
-  <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
 
   <script type="text/javascript">
     /* years of ex */
     $('.Years-of-excellence__slider').owlCarousel({
-      loop:true,
+      loop: true,
       nav: false,
       dots: true,
       autoplay: true,
       autoplayTimeout: 3000,
       item: 6,
-      margin:50,
+      margin: 50,
       responsive: {
         0: {
           items: 2
@@ -187,3 +229,6 @@
       }
     });
   </script>
+</body>
+
+</html>
