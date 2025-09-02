2739536d434210ea0f3f17c65d991d155bfbb768
unknown-dev-257 <ecommerce@premindustries.in>
2025-09-02 11:09:16 +0530

    Updated Corrugated Container page and sitemap

--- corrugated-containers.php
+++ corrugated-containers.php
@@ -1,78 +1,107 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Discover our strong corrugated containers for secure product packaging. Choose reliability with Prem Industries India Limited. Contact us now!">
-    <meta property="og:title" content="Corrugated Containers Manufacturer | Prem Industries India Limited">
-    <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
 
-    <title>Corrugated Containers Manufacturer</title>
-    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
-    <!-- Bootstrap core css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this site -->
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
-  </head>
-  <body>
-    <!-- header start -->
-    <?php include 'header.php';?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Discover our strong corrugated containers for secure product packaging. Choose reliability with Prem Industries India Limited. Contact us now!">
+  <meta property="og:title" content="Corrugated Containers Manufacturer | Prem Industries India Limited">
+  <meta name="google-site-verification" content="dy8TnUxfwAmXE1bQiSOV3s_IU8kpFCIp6TT8I7GSZL0" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSMKSG4F96"></script>
+  <script>window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-PSMKSG4F96'); </script>
+
+  <title>Corrugated Containers Manufacturer</title>
+  <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+  <!-- Bootstrap core css -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this site -->
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/media.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  <style>
+    h4 {
+      color: #14254C;
+    }
+
+    .bullet-point-new {
+      color: #14254C;
+    }
+
+    b {
+      color: #14254C;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- header start -->
+  <?php include 'header.php'; ?>
 
 
-    <div class="page-hero corrugated-Containers pageheade">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <h1>Eco-friendly Containers</h1>
-            <p>A Hygienic Way to Protect & Contain Products</p>
-          </div>
-          <div class="col-md-12">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Corrugated containers</li>
-              </ol>
-            </nav>
-          </div>
+  <div class="page-hero corrugated-Containers pageheade">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h1>Eco-friendly Containers</h1>
+          <p>A Hygienic Way to Protect & Contain Products</p>
+        </div>
+        <div class="col-md-12">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Corrugated containers</li>
+            </ol>
+          </nav>
         </div>
       </div>
+    </div>
   </div>
 
-  <section class="body__content padding page-body-content">
+  <section class="body__content pt-5 page-body-content">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="text-left mb-4">
+          <div class="text-left">
             <h3 class="wow fadeInUp">Delivering Diverse Solutions in a Single Range</h3>
-            <a href="contact-us.php" class="contact-add">
+            <!-- <a href="contact-us.php" class="contact-add">
               <i class="fa fa-envelope"></i>
               <span>Get more information</span>
-            </a>
+            </a> -->
           </div>
         </div>
       </div>
-      <div class="row mt-60">
+      <div class="row" style="text-align: justify">
         <div class="col-md-6 order2">
           <div class="body-content">
-            <p class="wow fadeInUp">The corrugated packaging at PREM is a leading vertically integrated manufacturing division in North India, offering a wide range of converting capabilities as well as high-quality corrugated materials and machines designed to protect, promote, deliver, and display your products.</p>
-            <p>Whether you require a one-of-a-kind shape, a specially reinforced structure, a shelf-ready solution, a simple one-color box, or a dynamic display, our skilled team creates and delivers solutions that are important to you by:</p>
-            <ul class="listing-content">
-              <li><i class="fa fa-arrow-right"></i> Lowering your total cost through the supply chain and developing sustainable solutions to reduce environmental impact throughout the package life-cycle </li>
-              <li><i class="fa fa-arrow-right"></i> Increasing sales through innovative designs and dynamic shelf appeal for unique consumer experiences. </li>
-              <li><i class="fa fa-arrow-right"></i> Reduce your risk with time-tested designs, sales support, unparalleled machine’s performance, and a coast-to-coast footprint. </li>
-              <li><i class="fa fa-arrow-right"></i> Increase your sustainability by using more renewable and recyclable resources. </li>
+            <p class="wow fadeInUp">The corrugated packaging at PREM is a leading vertically integrated manufacturing
+              division in North India, offering a wide range of converting capabilities as well as high-quality
+              corrugated materials and machines designed to protect, promote, deliver, and display your products.</p>
+            <p>Whether you require a one-of-a-kind shape, a specially reinforced structure, a shelf-ready solution, a
+              simple one-color box, or a dynamic display, our skilled team creates and delivers solutions that are
+              important to you by:</p>
+            <ul class="mt-0 mb-3">
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span class="bullet-point-new">⦿</span>
+                Lowering your total cost through the supply chain and developing
+                sustainable solutions to reduce environmental impact throughout the package life-cycle</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span class="bullet-point-new">⦿</span>
+                Increasing sales through innovative designs and dynamic shelf appeal
+                for unique consumer experiences.</li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span class="bullet-point-new">⦿</span>
+                Reduce your risk with time-tested designs, sales support,
+                unparalleled machine’s performance, and a coast-to-coast footprint.
+              </li>
+              <li style="font-size: 15px; font-weight: 500; line-height: 26px;"><span class="bullet-point-new">⦿</span>
+                Increase your sustainability by using more renewable and recyclable
+                resources.</li>
             </ul>
-            <p>Our integrated packaging solutions include the Packaging Consulting Group, which will evaluate your production line and recommend technology-based packaging efficiencies. Furthermore, we will suggest ways to maximise, simplify, or modify your container designs in order to optimise packaging and processes.</p>
+            <p>Our integrated packaging solutions include the Packaging Consulting Group, which will evaluate your
+              production line and recommend technology-based packaging efficiencies. Furthermore, we will suggest ways
+              to maximise, simplify, or modify your container designs in order to optimise packaging and processes.</p>
           </div>
         </div>
-        <div class="col-md-6 order1">
+        <div class="col-md-6 order1 mt-4">
           <div class="design-image wow" data-wow-delay="0.5s">
             <img class="image1" width="665" height="870" src="img/store.jpg" alt="Corrugated Containers" />
           </div>
@@ -88,7 +117,7 @@
     <h4 class="bold-title"></h4>
   </div>
 
-  <?php include 'contactsection.php';?>
-  
+  <?php include 'contactsection.php'; ?>
+
   <!-- header start -->
-  <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
