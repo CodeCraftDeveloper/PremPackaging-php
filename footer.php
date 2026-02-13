35ee56ccc547af725dab39e07355dc8ea5bef778
chandi977 <pappumahato000@gmail.com>
2026-02-13 12:37:26 +0530

    updates

--- footer.php
+++ footer.php
@@ -2,29 +2,30 @@
 .col-lg-3.col-md-6.harry ul li a {
     display: inline-block;
     float: left;
-    padding-right:20px;
+    padding-right: 20px;
     font-size: 25px;
 }
 
 
 .margin__top.pt-3.mobile a:hover {
-    color: #f96164!important;
+    color: #f96164 !important;
     text-decoration: underline;
 }
 
 .email a:hover {
-    color: #f96164!important;
+    color: #f96164 !important;
     text-decoration: underline;
 }
-</style>
-</head>
-<body>
-  <footer class="footer pb-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-		
-		<!--
+  </style>
+  </head>
+
+  <body>
+      <footer class="footer pb-5">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+
+                      <!--
 		
           <h3>How can we help you?</h3>
           <form class="contact-form" name="contact-form" action="" method="POST">
@@ -58,90 +59,108 @@
               </div>
             </div>
           </form>-->
-        </div>
-      </div>
-      <div class="row margin__top pt-3">
-        <div class="col-lg-3 col-md-6">
-          <h5 style="font-size: 18px">Packaging Solutions</h5>
-          <ul>
-            <li><a href="E-commerce-packaging.php" style="font-size: 16px">E-Commerce Packaging</a></li>
-            <li><a href="E-com-paper-bags.php" style="font-size: 16px">E-com Paper Bags </a></li>
-            <li><a href="food-packaging.php" style="font-size: 16px">Food Packaging</a></li>
-            <li><a href="luxury-packaging.php" style="font-size: 16px">Luxury Packaging</a></li>
-            <li><a href="commercial-printing.php" style="font-size: 16px">Commercial Printing</a></li>
-            <li><a href="electronics-packaging.php" style="font-size: 16px">Electronics Packaging</a></li>
-            <li><a href="beauty-personal-care.php" style="font-size: 16px">Beauty & Personal Care</a></li>
-            <li><a href="fmcg-packaging.php" style="font-size: 16px">FMCG Packaging</a></li>
-            <li><a href="healthcare-packaging.php" style="font-size: 16px">Healthcare Packaging</a></li>
-          </ul>
-          <ul>
-            <li><a href="#"></a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <a href="corrugated-packaging.php"><h5  style="font-size: 18px">Corrugated Packaging</h5></a>
-          <ul>
-            <li><a href="corrugated-containers.php" style="font-size: 16px">Corrugated Containers</a></li>
-            <li><a href="displays.php" style="font-size: 16px">Displays</a></li>          
-          </ul>
-          <a href="sustainability.php"><h5 style="font-size: 18px">Sustainability</h5></a>
-          <a href="packaging-technology.php"><h5 style="font-size: 18px" class="m-0">Packaging Technology</h5></a>
-          <ul>
-            <li><a href="#">&nbsp;</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <h5 style="font-size: 18px">Company Overview</h5>
-          <ul>
-            <li><a href="about-us.php" style="font-size: 16px">About Us</a></li>
-		<!--	 <li><a href="team.php">Our Team</a></li> -->
-			<li><a href="clients.php"  style="font-size: 16px">Our Clients</a></li>
-            
-            <!-- <li><a href="mission-vision.php">Mission & Vision</a></li> -->
-            <li><a href="careers.php" style="font-size: 16px">Careers</a></li>
-			<li><a href="quality-control.php" style="font-size: 16px">Quality Control</a></li>
-			<li><a href="research-development.php" style="font-size: 16px">Research & Development</a></li>
-      <li><a href="https://store.prempackaging.com" style="font-size: 16px">Shop</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-3 col-md-6 harry">
-		 <h5 style="font-size: 18px">About Company</h5>
-		<div class="margin__top pt-3 mobile">
-		<a style="color:rgba(255, 255, 255, 0.62); font-size:16px" href="tel:+91 84472 47227">+91 84472 47227</a>
-		</div>
-		
-		<div class="email">
-	    <a style="color:rgba(255, 255, 255, 0.62); font-size:16px" href="mailto: ecommerce@premindustries.in"> ecommerce@premindustries.in  </a>
-		</div>
-		
-		<div>
-		<h6 style="color:rgba(255, 255, 255, 0.62); padding-top:5px; font-size:16px"> C- 209, Bulandshahr Road, NH- 34,<br> Near Silver City Mall, Industries Area, Ghaziabad, Uttar Pradesh,<br> 
-Pin code: 201009
- </h6>
-		</div>
+                  </div>
+              </div>
+              <div class="row margin__top pt-3">
+                  <div class="col-lg-3 col-md-6">
+                      <h5 style="font-size: 18px">Packaging Solutions</h5>
+                      <ul>
+                          <li><a href="E-commerce-packaging.php" style="font-size: 16px">E-Commerce Packaging</a></li>
+                          <li><a href="E-com-paper-bags.php" style="font-size: 16px">E-com Paper Bags </a></li>
+                          <li><a href="food-packaging.php" style="font-size: 16px">Food Packaging</a></li>
+                          <li><a href="luxury-packaging.php" style="font-size: 16px">Luxury Packaging</a></li>
+                          <li><a href="commercial-printing.php" style="font-size: 16px">Commercial Printing</a></li>
+                          <li><a href="electronics-packaging.php" style="font-size: 16px">Electronics Packaging</a></li>
+                          <li><a href="beauty-personal-care-packaging.php" style="font-size: 16px">Beauty & Personal
+                                  Care</a>
+                          </li>
+                          <li><a href="fmcg-packaging.php" style="font-size: 16px">FMCG Packaging</a></li>
+                          <li><a href="healthcare-packaging.php" style="font-size: 16px">Healthcare Packaging</a></li>
+                      </ul>
+                      <ul>
+                          <li><a href="#"></a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                      <a href="corrugated-packaging.php">
+                          <h5 style="font-size: 18px">Corrugated Packaging</h5>
+                      </a>
+                      <ul>
+                          <li><a href="corrugated-containers.php" style="font-size: 16px">Corrugated Containers</a></li>
+                          <li><a href="displays.php" style="font-size: 16px">Displays</a></li>
+                      </ul>
+                      <a href="sustainability.php">
+                          <h5 style="font-size: 18px">Sustainability</h5>
+                      </a>
+                      <a href="packaging-technology.php">
+                          <h5 style="font-size: 18px" class="m-0">Packaging Technology</h5>
+                      </a>
+                      <ul>
+                          <li><a href="#">&nbsp;</a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-3 col-md-6">
+                      <h5 style="font-size: 18px">Company Overview</h5>
+                      <ul>
+                          <li><a href="about-us.php" style="font-size: 16px">About Us</a></li>
+                          <!--	 <li><a href="team.php">Our Team</a></li> -->
+                          <li><a href="clients.php" style="font-size: 16px">Our Clients</a></li>
+
+                          <!-- <li><a href="mission-vision.php">Mission & Vision</a></li> -->
+                          <li><a href="careers.php" style="font-size: 16px">Careers</a></li>
+                          <li><a href="quality-control.php" style="font-size: 16px">Quality Control</a></li>
+                          <li><a href="research-development.php" style="font-size: 16px">Research & Development</a></li>
+                          <li><a href="https://store.prempackaging.com" style="font-size: 16px">Shop</a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-3 col-md-6 harry">
+                      <h5 style="font-size: 18px">About Company</h5>
+                      <div class="margin__top pt-3 mobile">
+                          <a style="color:rgba(255, 255, 255, 0.62); font-size:16px" href="tel:+91 84472 47227">+91
+                              84472 47227</a>
+                      </div>
+
+                      <div class="email">
+                          <a style="color:rgba(255, 255, 255, 0.62); font-size:16px"
+                              href="mailto: ecommerce@premindustries.in"> ecommerce@premindustries.in </a>
+                      </div>
+
+                      <div>
+                          <h6 style="color:rgba(255, 255, 255, 0.62); padding-top:5px; font-size:16px"> C- 209,
+                              Bulandshahr Road, NH- 34,<br> Near Silver City Mall, Industries Area, Ghaziabad, Uttar
+                              Pradesh,<br>
+                              Pin code: 201009
+                          </h6>
+                      </div>
 
 
-    <div>
-		<p style="color:rgba(255, 255, 255, 0.62); padding-top:5px; font-size:16px"><b style="color:#FFF;"> Working Hours</b><br>9 AM - 6 PM<br>(Monday - Saturday) </p>
-		</div>
-		
-		
-          <ul>
-            <li><a href="https://www.facebook.com/PremIndustriesIndiaLimited/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="https://in.linkedin.com/company/prem-packaging" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
-			<li><a href="https://www.instagram.com/prem_packaging/" target="_blank"><i class="fab fa-instagram "></i></a></li>
-			<li><a href="https://www.youtube.com/@premindustries9251/videos" target="_blank"><i class="fab fa-youtube"></i></a></li>
-			<li><a href="https://api.whatsapp.com/send?phone=+918447247227&text=`Hi`" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
-			
-			
-			
-			
-          </ul>
-        </div>
-      </div>
-    </div>
-    
-    <!--
+                      <div>
+                          <p style="color:rgba(255, 255, 255, 0.62); padding-top:5px; font-size:16px"><b
+                                  style="color:#FFF;"> Working Hours</b><br>9 AM - 6 PM<br>(Monday - Saturday) </p>
+                      </div>
+
+
+                      <ul>
+                          <li><a href="https://www.facebook.com/PremIndustriesIndiaLimited/" target="_blank"><i
+                                      class="fab fa-facebook-f"></i></a></li>
+                          <li><a href="https://in.linkedin.com/company/prem-packaging" target="_blank"><i
+                                      class="fab fa-linkedin-in"></i></a></li>
+                          <li><a href="https://www.instagram.com/prem_packaging/" target="_blank"><i
+                                      class="fab fa-instagram "></i></a></li>
+                          <li><a href="https://www.youtube.com/@premindustries9251/videos" target="_blank"><i
+                                      class="fab fa-youtube"></i></a></li>
+                          <li><a href="https://api.whatsapp.com/send?phone=+918447247227&text=`Hi`" target="_blank"><i
+                                      class="fab fa-whatsapp"></i></a></li>
+
+
+
+
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
+          <!--
     
     <div class="copyright margin__top">
 	
@@ -149,13 +168,14 @@ Pin code: 201009
 	  
     </div>
     -->
-  </footer>
-  
-  <script src="js/jquery-3.1.1.min.js"></script>
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/wow.min.js"></script>
-  <script src="js/script.js"></script>
-</body>
-</html>
+      </footer>
+
+      <script src="js/jquery-3.1.1.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.fancybox.min.js"></script>
+      <script src="js/wow.min.js"></script>
+      <script src="js/script.js"></script>
+  </body>
+
+  </html>
